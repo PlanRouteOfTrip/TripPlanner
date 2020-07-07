886c732da3581509f9e2b6149ee580bc393d9bda
luky1300 <luky1300@luky1300s-macbook.local>
2020-07-07 15:42:55 -0400

    calculate time from start point to all middle points

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 let points = [];
+let pointsNameOnly = [];
+let startPoint = document.getElementById("yourLocation").value;
+
+document.getElementById("addStart").addEventListener("click", function (e) {
+  e.preventDefault();
+  startPoint = document.getElementById("yourLocation").value;
+});
 
 document.getElementById("addPoint").addEventListener("click", function (e) {
   e.preventDefault();
   let place = document.getElementById("point").value;
+  console.log(place)
   let minutes = document.getElementById("timeInPlace").value;
   points.push({ index: place, minsToSpend: minutes });
-//   console.log(points);
+  pointsNameOnly.push(place)
+  // console.log(points);
   document.getElementById("point").value = "";
   document.getElementById("timeInPlace").value = "";
 
@@ -25,7 +34,6 @@ document.getElementById("addPoint").addEventListener("click", function (e) {
       for (var i = 0; i < results.length; i++) {
         createMarker(results[i]);
       }
-
       map.setCenter(results[0].geometry.location);
     }
   });
@@ -42,3 +50,18 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 }
+
+document.getElementById("findTrips").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(`Starting point: ${startPoint}, points: ${pointsNameOnly}`)
+  var service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix(
+  {
+    origins: [startPoint],
+    destinations: pointsNameOnly,
+    travelMode: 'DRIVING'
+  }, 
+  function (response, status) {
+    console.log('from calculateTimeToFirstPoint', response, status)
+  });
+});
