951806f2727643cdedee45e4b1b75aa78de81456
Aleksandra Bardymova <bardymovasasha@gmail.com>
2020-07-08 13:24:16 -0400

    feat: calculating of difference between days in minutes

---
+++ date.js
@@ -0,0 +1,39 @@
+// let startDay
+// let startTime
+// let endDay
+// let endTime
+
+// document
+//   .getElementById("dateOfTripStart")
+//   .addEventListener("change", function () {
+//     startDay = document.getElementById("dateOfTripStart").value;
+//     console.log("this is start date", startDay);
+//   });
+
+// document.getElementById("startTime").addEventListener("change", function () {
+//   startTime = document.getElementById("startTime").value;
+//   console.log("this is start time", startTime);
+// });
+
+// document
+//   .getElementById("dateOfTripEnd")
+//   .addEventListener("change", function () {
+//     endDay = document.getElementById("dateOfTripEnd").value;
+//     console.log("this is end date", endDay);
+//   });
+
+// document.getElementById("endTime").addEventListener("change", function () {
+//   endTime = document.getElementById("endTime").value;
+//   console.log("this is end time", endTime);
+// });
+
+// function diff_hours(dt2, dt1) {
+//     let diff = (dt2.getTime() - dt1.getTime()) / 1000
+//     diff /= (60 * 60)
+//     return Math.abs(Math.round(diff))
+// }
+
+// dt1 = new Date(startDay + "T" + startTime)
+// dt2 = new Date(endDay + "T" + endTime)
+
+// console.log(diff_hours(dt2, dt1))
